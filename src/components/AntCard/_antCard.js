05d788895ba679a858b4964b2325ff1b0623ba03
Chris <13cryss@gmail.com>
2019-06-22 00:12:25 +0200

    [REFACTO] Reformat code

--- src/components/AntCard/_antCard.js
+++ src/components/AntCard/_antCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'antd'
+import {Card} from 'antd'
 
 const _antCard = () => (
     <Card title="Card title">Card content</Card>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import AntCard from './components/AntCard/_antCard';
 import './index.scss'
 
-const App = () => 
-  <div>
-    <AntCard />
-  </div>
+const App = () =>
+    <div>
+        <AntCard/>
+    </div>;
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'));
 
 if (module.hot) {
-  module.hot.accept()
+    module.hot.accept()
 }
